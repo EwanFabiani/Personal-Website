dd206b20b65a558341b72d599ccbe27741d1230e
EwanFabiani <ewan.fabiani.c@gmail.com>
2022-07-13 19:32:09 +0200

    Very small changes

--- script.js
+++ script.js
@@ -42,29 +42,32 @@ window.addEventListener('resize', function(event){
     var mediumStarsString = mediumStars.style.boxShadow;
     var bigStarsString = bigStars.style.boxShadow;
 
-    var smallStarsArray = smallStarsString.split(',');
-    var mediumStarsArray = mediumStarsString.split(',');
-    var bigStarsArray = bigStarsString.split(',');
+    var smallStarsArray = smallStarsString.split(', ');
+    var mediumStarsArray = mediumStarsString.split(', ');
+    var bigStarsArray = bigStarsString.split(', ');
 
-    smallStars.style.boxShadow = adjustStars(smallStarsString, smallAmount);
-    mediumStars.style.boxShadow = adjustStars(mediumStarsString, mediumAmount);
-    bigStars.style.boxShadow = adjustStars(bigStarsString, bigAmount);
+    smallStars.style.boxShadow = adjustStars(smallStarsString, smallAmount, 1);
+    mediumStars.style.boxShadow = adjustStars(mediumStarsString, mediumAmount, 2);
+    bigStars.style.boxShadow = adjustStars(bigStarsString, bigAmount, 3);
       
 
 });
 
-function adjustStars(string, amount) {
+function adjustStars(string, amount, index) {
     var newString = string;
-    var stars = string.split(',');
+    var stars = string.split(', ');
     var starsOldAmount = stars.length;
+    console.log("StarsOldAmount: " + starsOldAmount);
+    console.log("Amount: " + amount);
+    console.log("Index: " + index + "\n");
     if (starsOldAmount > amount) {
         for (var i = 0; i < starsOldAmount - amount; i++) {
             stars.pop();
         }
-        newString = stars.join(',');
+        newString = stars.join(', ');
     } else if (starsOldAmount < amount) {
         for (var i = 0; i < amount - starsOldAmount; i++) {
-            newString += ', '  + Math.random()*100 + 'vw '+ Math.random()*200+ 'vh #FFF';
+            newString += ', '  + Math.random()*100 + 'vw '+ Math.random()*200 + 'vh #FFF';
         }
         if (string.length===0) {newString = newString.substring(2);}
     } else {
@@ -74,9 +77,9 @@ function adjustStars(string, amount) {
 }
 
 function createStars(amount){
-    var stars = Math.random()*100 + 'vw '+ Math.random()*200+ 'vh #FFF';
+    var stars = Math.random()*100 + 'vw '+ Math.random()*200 + 'vh #FFF';
     for(var i = 0; i < amount; i++){
-        stars += ', '  + Math.random()*100 + 'vw '+ Math.random()*200+ 'vh #FFF';
+        stars += ', '  + Math.random()*100 + 'vw '+ Math.random()*200 + 'vh #FFF';
     }
     return stars;
 }
